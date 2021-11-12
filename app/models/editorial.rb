ec8cd148379bae7d2c1a3c23a88fab2b6a14eb67
JamesManguem <18090067@alumno.utmetropolitana.edu.mx>
2021-11-11 23:21:24 -0600

    ConfiguracionInicial

---
+++ app/models/editorial.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: editorials
+#
+#  id         :integer          not null, primary key
+#  country    :string
+#  name       :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Editorial < ApplicationRecord
+end
---
+++ test/models/editorial_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: editorials
+#
+#  id         :integer          not null, primary key
+#  country    :string
+#  name       :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require "test_helper"
+
+class EditorialTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

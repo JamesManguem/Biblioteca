23c5e3ba51c2bfa1ff18b9e1b43d07978c77e194
JamesManguem <18090067@alumno.utmetropolitana.edu.mx>
2021-11-17 23:55:17 -0600

    NewAuthorsViews

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,6 +1,18 @@
 class AuthorsController < ApplicationController
   before_action :find_author, only: [:show, :edit, :update, :destroy]
 
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.create(author_params)
+    @author.save
+    redirect_to @author
+
+  end
+
+
   def index
   @authors = Author.all
   end
@@ -8,8 +20,15 @@ class AuthorsController < ApplicationController
   def show
   end
 
+
+
   def find_author
     @author = Author.find(params[:id])
+  end
+
+  private
+  def author_params
+    params.require(:author).permit(:name,:language,:nationality)
   end
 
 end
